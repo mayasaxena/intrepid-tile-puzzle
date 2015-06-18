b7752cabcd112a287c95defa45662a4f68dc6511
Maya Saxena <maya.saxena@gmail.com>
2015-06-18 10:55:56 -0400

    Added main view controller'

--- intrepid-tile-puzzle/TPViewController.h
+++ intrepid-tile-puzzle/TPViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TPViewController.h
 //  intrepid-tile-puzzle
 //
 //  Created by Maya Saxena on 6/18/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TPViewController : UIViewController
 
 @end
-
---
+++ intrepid-tile-puzzle/TPViewController.m
@@ -0,0 +1,24 @@
+//
+//  TPViewController.m
+//  intrepid-tile-puzzle
+//
+//  Created by Maya Saxena on 6/18/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TPViewController.h"
+
+@interface TPViewController ()
+
+@end
+
+@implementation TPViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+
+@end
